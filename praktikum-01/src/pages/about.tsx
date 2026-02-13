0765127669c4fc544195907167e3864b5abedd3a
Aryan Saputra Rahmad <arfira1308@gmail.com>
2026-02-13 19:16:27 +0700

    praktikum 1: tugas 1

---
+++ praktikum-01/src/pages/about.tsx
@@ -0,0 +1,12 @@
+export default function About() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Halaman About</h1>
+      <hr />
+
+      <p><strong>Nama Mahasiswa:</strong> Aryan Saputra Rahmad</p>
+      <p><strong>NIM:</strong> 2341720022</p>
+      <p><strong>Program Studi:</strong> D4 Teknik Informatika</p>
+    </div>
+  )
+}
