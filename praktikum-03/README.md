54925c816d6d927e4fe5f4fa123aaca58190f27d
Aryan Saputra Rahmad <arfira1308@gmail.com>
2026-02-21 13:04:44 +0700

    praktikum 3 : Membuat Catch-All Route

--- praktikum-03/README.md
+++ praktikum-03/README.md
@@ -1,2 +1,6 @@
 1. Setup
 ![Setup](public/docs/setup.png)
+
+2. Membuat Catch-All Route 
+![lLangkah 2](public/docs/1.png)
+![Langkah 2](public/docs/2.png)
---
+++ praktikum-03/src/pages/shop/[slug].tsx
@@ -0,0 +1,18 @@
+import { useRouter } from "next/router";
+
+const HalamanToko = () => {
+  const { query } = useRouter();
+
+  const slug = query.slug as string[] | undefined;
+
+  return (
+    <div>
+      <h1>Halaman Toko</h1>
+      <p>
+        Toko: {slug ? `${slug[0]}-${slug[1]}` : "Tidak ada slug"}
+      </p>
+    </div>
+  );
+};
+
+export default HalamanToko;
