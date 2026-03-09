90830c499048235de5e1a2e8fecc75a540ebaf31
Aryan Saputra Rahmad <arfira1308@gmail.com>
2026-03-09 20:38:05 +0700

    praktikum 8 :  mplementasi getServerSideProps pada server.tsx

--- praktikum-08/README.md
+++ praktikum-08/README.md
@@ -2,3 +2,7 @@
 ![awal](public/docs/1.png)
 ![awal](public/docs/2.png)
 
+2. mplementasi getServerSideProps pada server
+![awal](public/docs/3.png)
+![awal](public/docs/4.png)
+
--- praktikum-08/src/pages/produk/server.tsx
+++ praktikum-08/src/pages/produk/server.tsx
@@ -1,12 +1,35 @@
 import TampilanProduk from "../../views/products";
 
-const HalamanProdukServer = () => {
+type ProductType = {
+  id: string;
+  name: string;
+  price: number;
+  image: string;
+  category: string;
+};
+
+const HalamanProdukServer = (props: { products: ProductType[] }) => {
+  const { products } = props;
+
   return (
     <div>
       <h1>Halaman Produk Server</h1>
-      <TampilanProduk products={[]} isLoading={false} />
+      <TampilanProduk products={products} isLoading={false} />
     </div>
   );
 };
 
 export default HalamanProdukServer;
+
+// Fungsi getServerSideProps akan dipanggil setiap kali halaman diakses
+// untuk mengambil data produk dari API sebelum halaman dirender
+export async function getServerSideProps() {
+  const res = await fetch("http://localhost:3000/api/products");
+  const response = await res.json();
+
+  return {
+    props: {
+      products: response.data || [],
+    },
+  };
+}
